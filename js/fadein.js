9eefea72bf48296c800334834ea4585125be6831
jdes96 <56548628+jdes96@users.noreply.github.com>
2024-04-11 17:45:54 +0100

    11April24 JS

--- js/fadein.js
+++ js/fadein.js
@@ -1,7 +1,23 @@
 $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop()
     var pageBottom = pageTop + $(window).height()
-    var tags = $("section")
+    var tags = $("section, .fadeIn, .fadeLeft, .fadeRight, .fadeUp")
+    
+    for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+    
+    if ($(tag).position().top < pageBottom) {
+    $(tag).addClass("visible")
+    } else {  $(tag).removeClass("visible")
+    }
+    }
+})
+
+
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $(".fadeUpDelayed")
     
     for (var i = 0; i < tags.length; i++) {
     var tag = tags[i]
